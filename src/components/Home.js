580263a39534ad934499fd1c663dc1f5cb119f8c
johannes <johannes.muljana@tafensw.edu.au>
2021-05-11 16:38:51 +1000

    add article view in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { Banner } from '../components/Banner'
-
+import { useState, useEffect} from 'react'
 
 const BannerStyle = {
     backgroundImage:"url(Banner.jpg)",
@@ -10,13 +10,39 @@ const BannerStyle = {
 }
 
 export function Home( props ) {
-    props.items().then( (articles) => {
-        console.log(articles)
+    // store articles
+    const [ articles, setArticles ] = useState([])
+
+    props.items().then( (data) => {
+       setArticles( data )
     } )
+
+    const ArticleView = ( props ) => {
+        return (
+            <div className="article">
+                <h3>{ props.article.title }</h3>
+                <p>{ props.article.author }</p>
+            </div>
+        )
+    }
+
+    const ArticlesList = ( props ) => {
+        if( articles ) {
+            const Collection = articles.map( (article) => {
+                return <ArticleView article={ article } />
+            })
+            return Collection
+        }
+        else {
+            return( <p>Sorry, no articles!</p>)
+        }
+    }
+
     return(
         <main className="home">
             <Banner text="Welcome" style={BannerStyle} />
             <h1>Home</h1>
+            <ArticlesList />
         </main>
     )
 }
