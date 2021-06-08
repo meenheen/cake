3a215321f34d4776a5acd9e177ea7e4462cafab2
johannes <johannes.muljana@tafensw.edu.au>
2021-06-08 17:08:08 +1000

    added url for cakes

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,34 +4,35 @@ import {About} from './About'
 import {Contact} from './Contact'
 import {Cakes} from './Cakes'
 // firebase
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/storage'
-import {firebaseConfig} from '../components/config/Config'
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+// import 'firebase/auth'
+// import 'firebase/storage'
+// import {firebaseConfig} from '../components/config/Config'
 
 export function Content(props) {
-    if( !firebase.apps.length ) {
-        firebase.initializeApp( firebaseConfig )
-    }
+    // if( !firebase.apps.length ) {
+    //     firebase.initializeApp( firebaseConfig )
+    // }
 
-    const getArticles = () => {
-        return new Promise( (resolve,reject) => {
-            let articles = []
-            const db = firebase.firestore()
-            db.collection('articles').get()
-            .then( (snapshot) => {
-                snapshot.forEach( (doc) => {
-                    articles.push( doc.data() )
-                })
-                resolve( articles )
-            })
-            .catch( (error) => {
-                reject( error.message )
-            })
-        } )
-    }
+    // const getArticles = () => {
+    //     return new Promise( (resolve,reject) => {
+    //         let articles = []
+    //         const db = firebase.firestore()
+    //         db.collection('articles').get()
+    //         .then( (snapshot) => {
+    //             snapshot.forEach( (doc) => {
+    //                 articles.push( doc.data() )
+    //             })
+    //             resolve( articles )
+    //         })
+    //         .catch( (error) => {
+    //             reject( error.message )
+    //         })
+    //     } )
+    // }
     
+    const postsURL = "https://public-api.wordpress.com/rest/v1.1/sites/txtpublish.wordpress.com/posts/"
 
     return (
         <Switch>
@@ -42,10 +43,10 @@ export function Content(props) {
                 <About />
             </Route>
             <Route path="/cakes">
-                <Cakes />
+                <Cakes url={postsURL} />
             </Route>
             <Route exact path="/">
-                <Home items={getArticles} />
+                <Home  />
             </Route>
         </Switch>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,41 +13,41 @@ export function Home( props ) {
     // store articles
     const [ articles, setArticles ] = useState([])
 
-    useEffect( () => {
-        if( articles.length === 0 ) {
-            props.items().then( (data) => {
-                setArticles( data )
-             } )
-        }
-    },[articles])
+    // useEffect( () => {
+    //     if( articles.length === 0 ) {
+    //         props.items().then( (data) => {
+    //             setArticles( data )
+    //          } )
+    //     }
+    // },[articles])
    
 
-    const ArticleView = ( props ) => {
-        return (
-            <div className="article">
-                <h3>{ props.article.title }</h3>
-                <p>{ props.article.author }</p>
-            </div>
-        )
-    }
+    // const ArticleView = ( props ) => {
+    //     return (
+    //         <div className="article">
+    //             <h3>{ props.article.title }</h3>
+    //             <p>{ props.article.author }</p>
+    //         </div>
+    //     )
+    // }
 
-    const ArticlesList = ( props ) => {
-        if( articles.length ) {
-            const Collection = articles.map( (article) => {
-                return <ArticleView article={ article } />
-            })
-            return Collection
-        }
-        else {
-            return( <p>Sorry, no articles!</p>)
-        }
-    }
+    // const ArticlesList = ( props ) => {
+    //     if( articles.length ) {
+    //         const Collection = articles.map( (article) => {
+    //             return <ArticleView article={ article } />
+    //         })
+    //         return Collection
+    //     }
+    //     else {
+    //         return( <p>Sorry, no articles!</p>)
+    //     }
+    // }
 
     return(
         <main className="home">
             <Banner text="Welcome" style={BannerStyle} />
             <h1>Home</h1>
-            <ArticlesList />
+            {/* <ArticlesList /> */}
         </main>
     )
 }
